eddc36b5346aa6cc52745a9233b37a1c3c17500f
Patton <pattongrooms@rocketmail.com>
2023-06-01 10:40:44 -0400

    commit 29

--- match-tracker/routes/gameinfos.js
+++ match-tracker/routes/gameinfos.js
@@ -2,4 +2,9 @@ const express = require('express')
 const router = express.Router
 const gameInfosCtrl = require('../controllers/gameinfos')
 
+// All routes in this file start with '/' (root)
+
 // POST /matches/:id/gameinfos
+router.post('/matches/:id/gameinfos', gameInfosCtrl.create)
+
+module.exports = router
