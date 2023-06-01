cbad5b30f9c4e9389f8e7c481136f748812593ac
Patton <pattongrooms@rocketmail.com>
2023-05-31 20:53:23 -0400

    commit 23

--- match-tracker/routes/matches.js
+++ match-tracker/routes/matches.js
@@ -1,22 +1,22 @@
 const express = require('express')
 const router = express.Router()
+const ensureLoggedIn = require('../config/ensureLoggedIn')
 
 // create controller module
 const matchesCtrl = require('../controllers/matches')
 
 // All actual paths start and default with "/matches"
+// GET /matches
+// could be just '/'
+router.get('/', matchesCtrl.index)
 
 // GET route for /matches/new
-router.get('/new', matchesCtrl.new)
+router.get('/new', ensureLoggedIn, matchesCtrl.new)
 
 // GET /matches/:id (show functionality) MUST be below new route
 router.get('/:id', matchesCtrl.show)
 
 // POST route for /movies
-router.post('/', matchesCtrl.create)
-
-// GET /matches
-// could be just '/'
-router.get('/', matchesCtrl.index)
+router.post('/', ensureLoggedIn, matchesCtrl.create)
 
 module.exports = router
