6b880f8624f3a078a01ba969b579242b55e1deff
Patton <pattongrooms@rocketmail.com>
2023-06-02 14:12:38 -0400

    commit 44

--- match-tracker/routes/index.js
+++ match-tracker/routes/index.js
@@ -4,7 +4,7 @@ const passport = require('passport')
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: "COACH'S TOOL" })
+  res.render('index', { title: "THE COACH'S TOOL" })
 })
 
 // Google OAuth login route
